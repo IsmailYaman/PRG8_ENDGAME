238226fe91767fc84be53ce9e310c233f072cfbb
Ismail <iso_yaman@outlook.com>
2023-04-17 02:17:34 +0200

    Last changes

--- predict.js
+++ predict.js
@@ -2,21 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const nn = ml5.neuralNetwork({ task: 'regression', debug: false })
 
     const calories = document.getElementById('calories')
-    const fat = document.getElementById('fat')
-    const carb = document.getElementById('carb')
+    // const fat = document.getElementById('fat')
+    // const carb = document.getElementById('carb')
 
     nn.load('./model/model.json', console.log("model Loaded"))
     // console.log(calories.value);
     async function makePrediction() {
         let cal = parseInt(calories.value)
-        let f = parseInt(fat.value)
-        let ca = parseInt(carb.value)
+        // let f = parseInt(fat.value)
+        // let ca = parseInt(carb.value)
         
-        if(cal >= 0 && f >= 0 && ca >= 0 ) {
-            const results = await nn.predict({ calories: cal, fat: f, carb: ca })
-            document.getElementById('result').innerHTML = `De geschatte proteine: ${parseFloat(results[0].protein)}`
+        if(cal >= 0 ) {
+            const results = await nn.predict({ calories: cal })
+            console.log(results);
+            document.getElementById('result').innerHTML = `De geschatte proteine: ${parseFloat(results[0].protein).toFixed(0)} gram.`
         } else {
-            document.getElementById('result').innerHTML = `Vul alle velden in`
+            document.getElementById('result').innerHTML = `Vul een geldig getal in.`
         }
     }
 
